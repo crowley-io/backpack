ed7d20fdcda114268cf1938ec2644f6c3229bb8d
Thomas LE ROUX <github@november-eleven.fr>
2016-03-17 15:54:47 +0100

    refactor(engine): Split Setup into several functions.

--- engine/setup.go
+++ engine/setup.go
@@ -11,13 +11,27 @@ import (
 )
 
 var (
-	ErrExecGidParamMatch = errors.New("gid doesn't match with given parameters")
-	ErrExecUidParamMatch = errors.New("uid doesn't match with given parameters")
+	// ErrExecGIDParamMatch is an error returned when the user's gid is not the required/requested one.
+	ErrExecGIDParamMatch = errors.New("gid doesn't match with given parameters")
+	// ErrExecUIDParamMatch is an error returned when the user's uid is not the required/requested one.
+	ErrExecUIDParamMatch = errors.New("uid doesn't match with given parameters")
 )
 
-func Setup(uid, gid int) error {
+func getUserPaths() (groupPath, passwdPath string, err error) {
 
-	os.Unsetenv("HOME")
+	if passwdPath, err = user.GetPasswdPath(); err != nil {
+		return "", "", err
+	}
+
+	if groupPath, err = user.GetGroupPath(); err != nil {
+		return "", "", err
+	}
+
+	return
+}
+
+func getUserExec(uid, gid int) (*user.ExecUser, error) {
+
 	id := fmt.Sprintf("%d:%d", uid, gid)
 
 	// Set up defaults.
@@ -27,40 +41,62 @@ func Setup(uid, gid int) error {
 		Home: "/",
 	}
 
-	passwdPath, err := user.GetPasswdPath()
+	passwdPath, groupPath, err := getUserPaths()
 	if err != nil {
-		return err
-	}
-
-	groupPath, err := user.GetGroupPath()
-	if err != nil {
-		return err
+		return nil, err
 	}
 
 	execUser, err := user.GetExecUserPath(id, &defaultExecUser, passwdPath, groupPath)
 	if err != nil {
-		return err
+		return nil, err
 	}
+
 	if execUser.Gid != gid {
-		return ErrExecGidParamMatch
+		return nil, ErrExecGIDParamMatch
 	}
 	if execUser.Uid != uid {
-		return ErrExecUidParamMatch
+		return nil, ErrExecUIDParamMatch
 	}
 
-	if err := syscall.Setgroups(execUser.Sgids); err != nil {
+	return execUser, nil
+}
+
+func setUserExecContext(u *user.ExecUser) error {
+
+	if err := syscall.Setgroups(u.Sgids); err != nil {
 		return err
 	}
 
-	if err := system.Setgid(execUser.Gid); err != nil {
+	if err := system.Setgid(u.Gid); err != nil {
 		return err
 	}
 
-	if err := system.Setuid(execUser.Uid); err != nil {
+	if err := system.Setuid(u.Uid); err != nil {
 		return err
 	}
 
-	if err := os.Setenv("HOME", execUser.Home); err != nil {
+	if err := os.Setenv("HOME", u.Home); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Setup will change running process to given uid and gid.
+// NOTE: You can't rollback to the previous user if its successful.
+func Setup(uid, gid int) error {
+
+	// Clear HOME since it will set later...
+	if err := os.Unsetenv("HOME"); err != nil {
+		return err
+	}
+
+	execUser, err := getUserExec(uid, gid)
+	if err != nil {
+		return err
+	}
+
+	if err := setUserExecContext(execUser); err != nil {
 		return err
 	}
 
