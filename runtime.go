5ecf6f5a5758827704ec4c760ec01d6f9ad33328
Thomas LE ROUX <github@november-eleven.fr>
2016-03-17 17:45:52 +0100

    docs: Add comments

--- runtime.go
+++ runtime.go
@@ -6,9 +6,14 @@ import (
 	"os"
 	"os/exec"
 	"syscall"
-	//"strings"
 )
 
+// handle is the backpack's supervisor:
+//   - First, it will create required user and group (defined by crowley-pack's environment variable).
+//   - Then, if any pre-hooks are defined, it will execute them in a child process.
+//   - After that, it will launch itself - also in a child process - in order to execute every command defined in
+//     the configuration file. Each command will be executed as the user created in the first step...
+//   - Finally, if any post-hooks are defined, it will execute them in a child process.
 func handle(c engine.Configuration) int {
 
 	// Create user and group.
@@ -43,6 +48,10 @@ func handle(c engine.Configuration) int {
 	return exitSuccess
 }
 
+// execute is the backpack's command wrapper:
+// It will switch to the required user (defined by crowley-pack's environment variable) and then execute the
+// specified command. Also, it will be no longer resident or involved in the process lifecycle at all once the
+// command is started.
 func execute(command string, args []string) int {
 
 	gid, err := engine.GroupID()
@@ -59,6 +68,7 @@ func execute(command string, args []string) int {
 		return exitErrUndefinedUserEnv
 	}
 
+	// Switch to user.
 	if err = engine.Setup(uid, gid); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		return exitErrSetupUser
@@ -72,6 +82,7 @@ func execute(command string, args []string) int {
 
 	args = append([]string{command}, args...)
 
+	// Execute given command.
 	if err = syscall.Exec(path, args, os.Environ()); err != nil {
 		fmt.Fprintln(os.Stderr, "Cannot execute: ", err)
 		return exitErrSyscallExec
