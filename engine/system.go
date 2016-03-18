ef68f21c2e139d8d4162dd8fd82afde77f5d5e99
Thomas LE ROUX <github@november-eleven.fr>
2016-03-18 21:43:17 +0100

    refactor(engine): Refactor Launch wrapper

--- engine/system.go
+++ engine/system.go
@@ -3,24 +3,18 @@ package engine
 import (
 	"os"
 	"os/exec"
+	"syscall"
 )
 
-// CmdParser is a function which must return an executable Cmd from a given command.
-type CmdParser func(string) *exec.Cmd
+// Launch will execute the given command without forking.
+func Launch(args []string) error {
 
-// Launch will execute all given commands, sequentially, and stop on the first command failure (if any).
-func Launch(commands []string, create CmdParser) error {
-
-	for _, command := range commands {
-
-		cmd := create(command)
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-
-		if err := cmd.Run(); err != nil {
-			return err
-		}
+	// Find command path.
+	path, err := exec.LookPath(args[0])
+	if err != nil {
+		return err
 	}
 
-	return nil
+	// Execute given command.
+	return syscall.Exec(path, args, os.Environ())
 }
