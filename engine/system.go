08c420bc8a41e8e93a3bf7e69cd70c1bcb47d170
Thomas LE ROUX <github@november-eleven.fr>
2016-03-21 18:09:00 +0100

    refactor(system): Add mocking system for Launch

--- engine/system.go
+++ engine/system.go
@@ -8,13 +8,19 @@ import (
 
 // Launch will execute the given command without forking.
 func Launch(args []string) error {
+	return launch(args, exec.LookPath, syscall.Exec)
+}
+
+func launch(args []string,
+	lookpath func(cmd string) (path string, err error),
+	exec func(arg0 string, argv []string, env []string) error) error {
 
 	// Find command path.
-	path, err := exec.LookPath(args[0])
+	path, err := lookpath(args[0])
 	if err != nil {
 		return err
 	}
 
 	// Execute given command.
-	return syscall.Exec(path, args, os.Environ())
+	return exec(path, args, os.Environ())
 }
